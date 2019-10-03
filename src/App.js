645342f75a6119c99a7cb1526e0fe6ff8a07618d
Sean Belverstone <seanbelverstone@gmail.com>
2019-10-03 18:18:25 -0500

    Got a bit carried away as per usual. Navbar is way complete, started then adding in Links and react-router-dom into and got a bit ahead of myself. Created files for Jumbotron and Wrapper and added them into App.js. Will push up this branch and carry on on another

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar";
+import BBCJumbo from "./components/Jumbotron";
 
 function App() {
   return (
-      <Navbar />
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={BBCJumbo}/>
+          {/* <Route exact path="/savedArticles" component={SavedArticles} /> */}
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,44 +1,28 @@
 import React from 'react';
 import {
-  Collapse,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
   Nav,
-  NavItem,
-  NavLink
+  NavItem
  } from 'reactstrap';
+ import { Link } from "react-router-dom";
+
 
 export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
   render() {
     return (
       <div>
         <Navbar color="light" light expand="md">
           <NavbarBrand href="/">BBC Mongo Web Scraper</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink href="/components/">Home</NavLink>
+                <Link to="/">Home</Link>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">Saved Articles</NavLink>
+                <Link to="/savedArticles">Saved Articles</Link>
               </NavItem>
             </Nav>
-          </Collapse>
         </Navbar>
       </div>
     );
